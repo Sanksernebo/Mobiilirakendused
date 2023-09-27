138838fb3d44f04ee889a4fa8afcc1b8493af16e
Sanksernebo <karl.sangernebo@voco.ee>
2023-09-27 13:55:08 +0300

    Authentication Screen Yl 3

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
 import Splash from "./src/screens/auth/Splash";
+import AuthHeader from "./src/components/AuthHeader";
+import Signup from "./src/screens/auth/SignUp/Signup";
 const App = () => {
 return (
   <SafeAreaView>
-    <Splash />
+    <Signup/>
   </SafeAreaView>
 );
 };
---
+++ src/utils/colors.js
@@ -0,0 +1,7 @@
+export const colors ={
+    blue: '#4F63AC',
+    white: '#FFF',
+    orange: '#FCA34D',
+    black: '#000',
+    grey: '#8D9BB%'
+}
