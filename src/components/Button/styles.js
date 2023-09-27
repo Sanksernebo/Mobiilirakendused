4f525df0bea4ed5285551c5c2ac52b3ee8365bda
Sanksernebo <karl.sangernebo@voco.ee>
2023-09-27 12:33:43 +0300

    create project basic files

---
+++ src/components/Button/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#4F63AC',
+        paddingHorizontal: 8,
+        paddingVertical:20,
+        borderRadius:8,
+        marginTop:55
+    },
+    title: {
+        color:'#FFF',
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight:'bold',
+
+    }
+})
---
+++ src/screens/auth/Splash/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+    Text,
+    View,
+    Image 
+} from "react-native";
+import Button from "../../../components/Button"
+import Link from "../../../components/Link";
+import {styles} from "./styles";
+
+const Splash = () => {
+    return (
+        <View style={styles.container}>
+            <Image resizeMode="contain" style={styles.image} source={require('../../../assets/splash_image.png')}/>
+            <Text style= {styles.title} >You'll Find </Text>
+            <Text style={[styles.title,styles.innerTitle]}>All you need </Text>
+            <Text style={styles.title}>Here!</Text>
+
+            <Button title="Sign Up"/>
+            <Link title="Sign in"></Link>
+        </View>
+    )
+}
+export default Splash
