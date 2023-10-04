ac48806d6c417802b34fc19ec986c63b19af3494
Sanksernebo <karl.sangernebo@voco.ee>
2023-10-04 10:47:20 +0300

    7. Google Login Done

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { SafeAreaView } from "react-native";
 import Splash from "./src/screens/auth/Splash";
 import AuthHeader from "./src/components/AuthHeader";
 import Signup from "./src/screens/auth/SignUp/Signup";
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import Config from "react-native-config";
+
+GoogleSignin.configure({
+  webClientId: '651630562809-vbgjlqqq7ihiml4fua3fp34c6v42pv60.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+  offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+  iosClientId: '651630562809-3h8mhdmsio6uqajf6hmot0e66llllmot.apps.googleusercontent.com', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+});
+
+
+const REVERSED_CLIENT_ID='com.googleusercontent.apps.651630562809-3h8mhdmsio6uqajf6hmot0e66llllmot'
+
+
 const App = () => {
+  useEffect(() =>{
+    GoogleSignin.configure({
+      scopes:['https://www.googleapis.com/auth/drive.readonly'],
+      webClientId: Config.WEB_CLIENT_ID,
+      offlineAccess:true,
+      iosClientId: Config.IOS_CLIENT_ID
+    })
+  },[])
 return (
   <SafeAreaView>
     <Signup/>
@@ -11,4 +32,4 @@ return (
 );
 };
 
-export default App
+export default React.memo(App)
--- src/components/GoogleLogin/style.js
+++ src/components/GoogleLogin/style.js
@@ -4,17 +4,13 @@ import { colors } from "../../utils/colors";
 
 export const styles =StyleSheet.create({
     container: {
-        backgroundColor: colors.darkGray,
-        borderRadius: 14,
-        width: '45%',
+        width: '100%',
         alignSelf: 'center',
         justifyContent: 'center',
         alignItems: 'center',
-        padding: 16,
         marginBottom: 50
     },
     image: {
-        width: 30,
-        height: 30
+
     }
 })
