e7f07fdd22472ef86e374020bf8f432bd7a1258a
Sanksernebo <karl.sangernebo@voco.ee>
2023-10-11 09:37:57 +0300

    Categoreis WIP

---
+++ src/components/Header/index.js
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+import {Pressable, Image, Text, View} from "react-native";
+import Input from "../Input";
+import {styles} from "./styles"
+
+const Header = ({title, onBackPress, onLogout, showBack, showSearch, showLogout }) => {
+    const [showSearchIput, setShowSearchInput] = useState(false)
+
+    const onSearch = () => {
+        setShowSearchInput(search => !search)
+    }
+    return(
+        <View>
+            <View style={styles.container}>
+                {showBack ? (
+                    <Pressable hitSlop={20} onPress={onBackPress}>
+                        <Image style={styles.icon} source={require('../../assets/back.png')} />
+                    </Pressable>
+                ) : showSearch ? (
+                    <Pressable hitSlop={20} onPress={onSearch}>
+                        <Image style={styles.icon} source={require('../../assets/search.png')} />
+                    </Pressable>
+                ) : <View style={styles.space} />
+                }
+            <Text style={styles.title}> {title} </Text>
+            {
+                showLogout ? (
+                    <Pressable hitSlop={20} onPress={onLogout}>
+                        <Image style={styles.icon} source={require('../../assets/Logout.png')} />
+                    </Pressable>
+                ) : <View style={styles.space} />
+            }
+            </View>
+            {
+                showSearchIput ? (
+                    <Input placeholder="Type your keyword" />
+                ) : null
+            }
+        </View>
+    )
+}
+export default React.memo(Header)
--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {styles} from "./styles"
- 
+import Header from "../../../components/Header";
+import { categories } from "../../../data/categories";
+
 const Home = () => {
+    const renderCategoryItem = ({item}) => {
+        console.log('item => ', item)
+        return(
+            <Text>{item.title}</Text>
+        )
+    }
+
     return(
         <SafeAreaView>
             <View style={styles.container}>
+                <Header showSearch={true} title="Find All You Need" />
+                <FlatList data={categories} renderItem={renderCategoryItem} keyExtractor={(item, index) =>String(index)} />
                 <Text>Home</Text>
             </View>
         </SafeAreaView>
